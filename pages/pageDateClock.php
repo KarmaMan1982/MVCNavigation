5f84474ac98495ae1e25d9c5884cf1aacc8e6ce5
KarmaMan1982 <markuseberherr@googlemail.com>
2017-10-27 14:27:44 +0200

    Update pageDateClock.php

--- pages/pageDateClock.php
+++ pages/pageDateClock.php
@@ -2,3 +2,4 @@
 echo 'pageDateClock';
 ?>
 <input name="basic_example_1" id="basic_example_1" type="text">
+<script type="text/javascript" language="JavaScript"> $("#basic_example_1").datetimepicker(); </script>
